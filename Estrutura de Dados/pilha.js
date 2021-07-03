fe8290d501378c7cfb27aa25b524f5190ccdd24e
Suzi Nogueira <asuzinogueira@gmail.com>
2021-07-03 00:11:08 -0300

    finalização do módulo Extra - Estrutura de Dados

---
+++ Estrutura de Dados/pilha.js
@@ -0,0 +1,57 @@
+var elementos = [];
+var topo = -1;
+const MAX = 10;
+
+function push(num){
+    if(topo < MAX){
+        topo = topo+1;
+        elementos[topo] = num;
+    }
+    else{
+        console.log("Pilha está cheia!");
+    }
+}
+
+function pop(){
+    if(topo != -1){
+        let num = elementos[topo];
+        topo = topo -1;
+        return num;
+    }
+    else{
+        console.log("Pilha está vazia !");
+    }
+}
+
+function estaVazia(){
+    return topo == -1;
+}
+
+var numDecimal = 10;
+var resto;
+
+console.log("Hora de empilhar.. ");
+while (numDecimal != 0){
+    resto = parseInt(numDecimal % 2);
+    push(resto);
+    console.log(resto);
+    numDecimal = parseInt(numDecimal / 2);
+}
+
+console.log("Desempilhando tudooo...")
+while(!estaVazia()){
+    console.log(pop());
+}
+
+// execução
+
+
+// push(10);
+// push(20);
+// push(30);
+
+// console.log(elementos);
+
+// console.log(pop());
+// console.log(pop());
+// console.log(pop());
